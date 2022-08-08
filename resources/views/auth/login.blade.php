98c59b828a59f369d343c7165f50fbed54e4e4a3
F4Ray <sharinnegan71@gmail.com>
2022-08-08 14:29:21 +0700

    fix front end login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <!-- <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember"
@@ -55,7 +55,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
@@ -64,9 +64,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
-                                </a>
+                                </a> -->
                                 @endif
                             </div>
                         </div>
--- resources/views/layouts/appOld.blade.php
+++ resources/views/layouts/appOld.blade.php
@@ -63,12 +63,6 @@
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @endif
-
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
